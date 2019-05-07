9cdcb60f14099f48d392577a630f8b56f3fcb2e9
liujiabin <15031384078@163.com>
2019-05-07 15:33:08 +0800

    insert sql and form

---
+++ app/src/main/java/sjzc/edu/cn/kuoutianqi/MainActivity.java
@@ -0,0 +1,22 @@
+package sjzc.edu.cn.kuoutianqi;
+
+import android.app.AlarmManager;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
